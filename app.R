f7c927516b80c3beb133c061a49fb4e649b3f564
mzaborek <maciekzaborek@o2.pl>
2018-01-03 20:03:23 +0100

    Added data source filtration

--- app.R
+++ app.R
@@ -2,18 +2,25 @@ library(shiny)
 library(ggplot2)  # for the diamonds dataset
 
 #data
-chr <- c("chr1")
-Start <- c(1000)
-Stop <- c(1200)
-CNV_type <- c("Del")
-Quality <- c(0.99)
-Source <- c("Ref")
+chr <- c("chr1", "chr2")
+Start <- c(1000, 200)
+Stop <- c(1200, 400)
+CNV_type <- c("Del", "Copy")
+Quality <- c(0.99, 0.35)
+Source <- c("Ref", "Alg1")
 
 cnvData <- data.frame(chr, Start, Stop, CNV_type, Quality, Source)
 
 #UI
 ui <- fluidPage(
   titlePanel("Examples of DataTables"),
+  fluidRow(
+    column(4,
+           selectInput("source",
+                       "Source:",
+                       c("All",
+                         unique(as.character(cnvData$Source)))))
+  ),
   fluidRow(DT::dataTableOutput("mytable1"))
 )
 
@@ -21,7 +28,14 @@ ui <- fluidPage(
 #Server
 server <- function(input, output) {
   output$mytable1 <- DT::renderDataTable({
-    DT::datatable(cnvData, options = list(orderClasses = TRUE))
+    DT::datatable({
+      data<-cnvData
+      if(input$source != "All"){
+        data<-data[data$Source == input$source,]
+      }
+      data
+      }, 
+      options = list(orderClasses = TRUE))
   })
 }
 
